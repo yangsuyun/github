1ee177f1c8fe37acc77d3c50361f1598ebbe10c9
yangsuyun <514671562@qq.com>
2016-10-27 20:48:11 +0800

    注释

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>无标题文档</title>
+</head>
+<style type="text/css">
+	div{
+		border:1px solid #ff0000;	
+	}
+</style>
+<script type="text/javascript">
+	function demo(){
+		alert(123);	
+	}
+	demo();
+</script>
+<body>
+<?php
+	echo 123;
+	$demo = 11111;
+	echo $demo;
+	echo "<br />";
+	$test = "aaaaaaaa";
+	$demo1= "bbbbbbbbbbbbb";
+	echo $test.$demo1;
+?>
+<script language="php">
+</script>
+<div>aaaa</div>
+</body>
+</html>
